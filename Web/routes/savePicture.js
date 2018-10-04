411b9f2f3e839095bbb6f656754695d4e188966a
공정훈 <rhdtl78@gmail.com>
2018-10-04 21:09:47 +0900

    Image Recieve Server

---
+++ Web/routes/savePicture.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var uploadDir = multer({storage: storage});
+/* GET home page. */
+router.post('/', uploadDir.single('file'), function (req, res, next) {
+  // TODO: 저장된 이미지를 학습, 식별하기.
+
+  res.send({message : "image uploaded"});
+});
+
+router.get('/', function(req, res, next) {
+
+});
+
+
+module.exports = router;
